a40fa03da95b2e2f4bbfcc9016950398740a5db3
bulutluoz <abbulutluoz@gmail.com>
2025-02-03 10:47:00 +0100

    Ders04 sonu

---
+++ src/ders04_forLoops/C06_SayininRakamlarToplaminiBulma.java
@@ -0,0 +1,45 @@
+package ders04_forLoops;
+
+import java.util.Scanner;
+
+public class C06_SayininRakamlarToplaminiBulma {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan pozitif bir tamsayi alip, rakamlar toplamini asagidaki gibi yazdirin.
+        // input : 1453 output => 1+4+5+3 = 13
+        // output :  "Girilen 1453 sayisinin rakamlar toplami : 13"
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Rakamlar toplamini bulmak icin pozitif bir tamsayi giriniz...");
+
+        int sayi = scanner.nextInt();
+
+        int basamakSayisi = (sayi+"").length();
+
+        int birlerBasamagi = 0;
+        int rakamlarToplami = 0 ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
