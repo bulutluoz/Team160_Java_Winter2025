719fdf9e0a4e23f346bb1121a472f654866658e8
bulutluoz <abbulutluoz@gmail.com>
2025-03-10 10:04:19 +0100

    Ders09 sonu

--- src/ders09_maps/C01_MapOlusturma.java
+++ src/ders09_maps/C01_MapOlusturma.java
@@ -25,6 +25,16 @@ public class C01_MapOlusturma {
 
         Map<Integer,String> ogrenciMap = new HashMap<>();
 
+        ogrenciMap.put(101,"Ali-Can-10-H-MF");
+        ogrenciMap.put(102,"Veli-Cem-10-K-TM");
+        ogrenciMap.put(103,"Ali-Cem-11-K-TM");
+        ogrenciMap.put(104,"Ayse-Can-10-H-MF");
+        ogrenciMap.put(105,"Sevgi-Cem-11-M-TM");
+        ogrenciMap.put(106,"Sevgi-Can-10-K-MF");
+        ogrenciMap.put(107,"Esra-Han-11-M-SOZ");
+
+
+
 
 
     }
