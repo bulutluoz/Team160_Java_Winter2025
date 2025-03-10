0ad448a40e17adaa1682819d1fd16143ac739b5b
bulutluoz <abbulutluoz@gmail.com>
2025-03-10 09:51:09 +0100

    Ders09 sonu

---
+++ src/ders09_maps/C01_MapOlusturma.java
@@ -0,0 +1,31 @@
+package ders09_maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class C01_MapOlusturma {
+
+    public static void main(String[] args) {
+        /*
+            Bir Map olusturmadan once
+            icine koyacagimiz datalari inceleyip
+            bazi kararlar vermeliyiz
+            1- Tum elemanlar hangi ortak ozellikleri tasiyor
+                no, isim, soyisim, sinif, sube, bolum
+            2- Tutacagimiz bilgilerden hangileri unique ve hangisi key olarak belirlenecek
+                key olarak no kullanilacak
+            3- key icin ayirdigimiz bilgi haric, geriye kalan bilgileri
+               hangi Java data turunu kullanarak TEK BIR value haline getirecegiz?
+                Tek bir String olarak birlestirecegiz
+            4- value icinde birden fazla bilgi olacagi icin
+               bu bilgilere sonradan erisebilmek adina
+               bu bilgileri birbirinden ayirabilecegimiz bir yontem belirlemek
+                aralarina - kullanacagiz
+         */
+
+        Map<Integer,String> ogrenciMap = new HashMap<>();
+
+
+
+    }
+}
